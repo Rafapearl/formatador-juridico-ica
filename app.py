cfa174b9799d9ea15721bf4b16ac2dc8215e4de8
RafaeFreitas <82437497+Rafapearl@users.noreply.github.com>
2025-10-08 20:15:42 -0300

    Update app.py
    identacao citacao

--- app.py
+++ app.py
@@ -323,7 +323,7 @@ def formatar_documento(doc_entrada, doc_saida_path, logo_path=None, debug_mode=F
                               espacamento_depois=6, recuo_lista=True)
         
         elif tipo == 'citacao':
-    aplicar_formatacao_paragrafo(p, alinhamento='justify', negrito=False,
+            aplicar_formatacao_paragrafo(p, alinhamento='justify', negrito=False,
                               italico=True, tamanho_fonte=11,  # Fonte menor e itálico
                               espacamento_antes=6, espacamento_depois=6,
                               recuo_lista=True)  # Com recuo para destacar
