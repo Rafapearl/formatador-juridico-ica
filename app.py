a2d37817007d2ba916f62934c689187ddbe47091
RafaeFreitas <82437497+Rafapearl@users.noreply.github.com>
2025-10-14 20:05:04 -0300

    Update app.py
    identacao2

--- app.py
+++ app.py
@@ -275,7 +275,7 @@ def formatar_documento(doc_entrada, doc_saida_path, logo_path=None, debug_mode=F
     criar_cabecalho(doc_novo, logo_path)
 
     em_pedidos = False # Controle da seção de Pedidosif texto == '' and em_pedidos:
-        if texto == '' and em_pedidos:
+    if texto == '' and em_pedidos:
         em_pedidos = False  # Desativa a seção de pedidos ao encontrar parágrafo vazio
     
     # Processar cada parágrafo do documento original
